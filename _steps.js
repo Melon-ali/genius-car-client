f9a477eb17f8110718b34d983783aa502baa7d7b
Melon-ali <86640181+Melon-ali@users.noreply.github.com>
2023-06-09 22:29:00 +0600

    genius car update

---
+++ _steps.js
@@ -0,0 +1,6 @@
+// step-1: 
+// * install react-router-dom
+// * TailwindCss framework --> framework-guides --> create-react-app -->npm install -D tailwindcss --> npx tailwindcss init --> content: [ "./src/**/*.{js,jsx,ts,tsx}], --> @tailwind base; @tailwind components; @tailwind utilities;
+
+// step-2: react-router-dom --> tutorial --> app.js
+// 

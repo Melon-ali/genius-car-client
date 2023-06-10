7e61ef0987011483e53e7d901e3e98a7e43b7694
Melon-ali <86640181+Melon-ali@users.noreply.github.com>
2023-06-11 01:10:16 +0600

    genius car added

--- _steps.js
+++ _steps.js
@@ -4,4 +4,4 @@
 
 // step-2: react-router-dom --> tutorial --> app.js
 
-//step-3: console.firebase.com --> create app --> app name --> app name --> src --> create file name --> firebase --> firebase.config.js --> export default app; --> package.json click --> .env.local --> all api key past --> REACT_APP_apikey= 
+//step-3: a) console.firebase.com --> create app --> app name --> app name --> src --> create file name --> firebase --> firebase.config.js --> export default app; --> package.json click --> .env.local --> all api key past --> REACT_APP_apikey= --> b) start authentication --> build --> authentication -->sing provider --> src --> create folder contexts --> create folder AuthProvider --> create file AuthProvider.js --> c) index.js --> // <AuthProvider> <App /> </AuthProvider> 

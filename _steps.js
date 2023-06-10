02460b37785f0e9096a84244ac1efed1b1969105
Melon-ali <86640181+Melon-ali@users.noreply.github.com>
2023-06-10 13:28:32 +0600

    genius car login added

--- _steps.js
+++ _steps.js
@@ -3,4 +3,5 @@
 // * TailwindCss framework --> framework-guides --> create-react-app -->npm install -D tailwindcss --> npx tailwindcss init --> content: [ "./src/**/*.{js,jsx,ts,tsx}], --> @tailwind base; @tailwind components; @tailwind utilities;
 
 // step-2: react-router-dom --> tutorial --> app.js
-// 
+
+//step-3: console.firebase.com --> create app --> app name --> app name --> src --> create file name --> firebase --> firebase.config.js --> export default app; --> package.json click --> .env.local --> all api key past --> REACT_APP_apikey= 
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_projectId,
+  projectId: process.env.REACT_APP_storageBucket,
+  storageBucket:process.env.REACT_APP_messagingSenderId,
+  messagingSenderId: process.env.REACT_APP_apiKey,
+  appId: process.env.REACT_APP_appId
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
